cebba5d48ba26bc095be3ee07d103f2910a2fbab
user-PC\user <k.ken0990@hotmail.co.jp>
2017-06-28 15:28:17 +0900

    バックグラウンドの更新

--- FrameWork/FrameWork/Fighting/Main/SceneManager/SceneBase/GameScene/ObjectManager/ObjectBase/GameObjectBase/BackGround/BackGround.cpp
+++ FrameWork/FrameWork/Fighting/Main/SceneManager/SceneBase/GameScene/ObjectManager/ObjectBase/GameObjectBase/BackGround/BackGround.cpp
@@ -14,12 +14,17 @@ const D3DXVECTOR2 BackGround::m_Rect = D3DXVECTOR2(1350, 1080);
 
 
 BackGround::BackGround(int _textureIndex) :
-m_TextureIndex(_textureIndex)
+m_TextureIndex(_textureIndex),
+m_Alfa(1.f),
+m_Alfa2(1.f),
+m_Alfa3(0.f),
+m_Alfa4(0.f),
+m_Alfa5(0.f)
 {
-	//SINGLETON_INSTANCE(Lib::TextureManager).Load("Resource/test_009.png", &m_TextureIndex);
-	//SINGLETON_INSTANCE(Lib::TextureManager).Load("Resource/test_010.png", &m_TextureIndex);
-	//SINGLETON_INSTANCE(Lib::TextureManager).Load("Resource/test_011.png", &m_TextureIndex);
-	SINGLETON_INSTANCE(Lib::TextureManager).Load("Resource/test_003.png", &m_TextureIndex);
+	SINGLETON_INSTANCE(Lib::TextureManager).Load("Resource/test_009.png", &m_PurpleSky);
+	SINGLETON_INSTANCE(Lib::TextureManager).Load("Resource/test_010.png", &m_RedSky);
+	SINGLETON_INSTANCE(Lib::TextureManager).Load("Resource/test_011.png", &m_OrangeSky);
+	SINGLETON_INSTANCE(Lib::TextureManager).Load("Resource/test_003.png", &m_BlueSky);
 
 	RECT ClientRect = SINGLETON_INSTANCE(Lib::Window).GetWindowSize();
 	m_Pos = D3DXVECTOR2(static_cast<float>(ClientRect.right / 2), static_cast<float>(ClientRect.bottom / 2));
@@ -39,6 +44,11 @@ m_TextureIndex(_textureIndex)
 		m_WidthTime = 30;
 	});
 
+	m_pVertex2 = new Lib::Vertex2D(
+		SINGLETON_INSTANCE(Lib::DX11Manager).GetDevice(),
+		SINGLETON_INSTANCE(Lib::DX11Manager).GetDeviceContext(),
+		SINGLETON_INSTANCE(Lib::Window).GetWindowSize());
+	m_pVertex2->Init(&m_Rect, m_pAnimUvController->GetUV());
 }
 
 BackGround::~BackGround()
@@ -48,8 +58,19 @@ BackGround::~BackGround()
 		m_pVertex->Release();
 		Lib::SafeDelete(m_pVertex);
 	}
-	
+
+	if (m_pVertex2 != NULL)
+	{
+		m_pVertex2->Release();
+		Lib::SafeDelete(m_pVertex2);
+	}
+
 	Lib::SafeDelete(m_pAnimUvController);
+
+	SINGLETON_INSTANCE(Lib::TextureManager).ReleaseTexture(m_BlueSky);
+	SINGLETON_INSTANCE(Lib::TextureManager).ReleaseTexture(m_OrangeSky);
+	SINGLETON_INSTANCE(Lib::TextureManager).ReleaseTexture(m_RedSky);
+	SINGLETON_INSTANCE(Lib::TextureManager).ReleaseTexture(m_PurpleSky);
 }
 
 
@@ -59,10 +80,67 @@ BackGround::~BackGround()
 
 void BackGround::Update()
 {
+	m_EnemyLine = SINGLETON_INSTANCE(GameDataManager).GetCurrentEnemyLine();
+
+	//m_Alfa = m_AfterAlfa - m_BeforeAlfa;
+	if (m_EnemyLine > 14)
+	{
+		m_Alfa2 -= 0.001f;
+		m_Alfa3 += 0.001f;
+	}
+	if (m_EnemyLine == 14)
+	{
+		m_Alfa2 = 0.f;
+		m_Alfa3 = 1.f;
+	}
+	if (m_EnemyLine < 14 && m_EnemyLine > 8)
+	{
+		m_Alfa3 -= 0.001f;
+		m_Alfa4 += 0.001f;
+	}
+	if (m_EnemyLine == 8)
+	{
+		m_Alfa3 = 0.f;
+		m_Alfa4 = 1.f;
+	}
+	if (m_EnemyLine < 8 && m_EnemyLine > 2)
+	{
+		m_Alfa4 -= 0.001f;
+		m_Alfa5 += 0.001f;
+	}
+	if (m_EnemyLine == 2)
+	{
+		m_Alfa4 = 0.f;
+		m_Alfa5 = 1.f;
+	}
 }
 
 void BackGround::Draw()
 {
 	Vibration();
 	m_pVertex->Draw(&D3DXVECTOR2(m_Pos.x + m_WidthVibValue, m_Pos.y), m_pAnimUvController->GetUV());
+	if (m_EnemyLine >= 14)
+	{
+		m_pVertex->Draw(&D3DXVECTOR2(m_Pos.x + m_WidthVibValue, m_Pos.y), m_pAnimUvController->GetUV(), m_Alfa2);
+		m_pVertex->SetTexture(
+			SINGLETON_INSTANCE(Lib::TextureManager).GetTexture(m_PurpleSky));
+	}
+	if (m_EnemyLine >= 8)
+	{
+		m_pVertex2->Draw(&D3DXVECTOR2(m_Pos.x + m_WidthVibValue, m_Pos.y), m_pAnimUvController->GetUV(), m_Alfa3);
+		m_pVertex2->SetTexture(
+			SINGLETON_INSTANCE(Lib::TextureManager).GetTexture(m_RedSky));
+	}
+	if (m_EnemyLine <= 14 && m_EnemyLine >= 8)
+	{
+		m_pVertex->Draw(&D3DXVECTOR2(m_Pos.x + m_WidthVibValue, m_Pos.y), m_pAnimUvController->GetUV(), m_Alfa4);
+		m_pVertex->SetTexture(
+			SINGLETON_INSTANCE(Lib::TextureManager).GetTexture(m_OrangeSky));
+	}
+	if (m_EnemyLine <= 8 && m_EnemyLine >= 0)
+	{
+		m_pVertex2->Draw(&D3DXVECTOR2(m_Pos.x + m_WidthVibValue, m_Pos.y), m_pAnimUvController->GetUV(), m_Alfa5);
+		m_pVertex2->SetTexture(
+			SINGLETON_INSTANCE(Lib::TextureManager).GetTexture(m_BlueSky));
+	}
 }
--- FrameWork/FrameWork/Fighting/Main/SceneManager/SceneBase/GameScene/ObjectManager/ObjectBase/GameObjectBase/BackGround/BackGround.h
+++ FrameWork/FrameWork/Fighting/Main/SceneManager/SceneBase/GameScene/ObjectManager/ObjectBase/GameObjectBase/BackGround/BackGround.h
@@ -8,6 +8,7 @@
 #include "Vertex2D/Vertex2D.h"
 #include "Animation/AnimUvController.h"
 #include "../GameObjectBase.h"
+#include "../../../../../GameDataManager/GameDataManager.h"
 
 class BackGround : public GameObjectBase
 {
@@ -38,10 +39,21 @@ private:
 
 	Lib::AnimUvController* m_pAnimUvController;
 	Lib::Vertex2D*		   m_pVertex;
+	Lib::Vertex2D*		   m_pVertex2;
 	D3DXVECTOR2			   m_Pos;
 	int					   m_TextureIndex;
 
-};
+	int					   m_BlueSky;
+	int					   m_PurpleSky;
+	int					   m_OrangeSky;
+	int					   m_RedSky;
 
+	int					   m_EnemyLine;
+	float				   m_Alfa;
+	float				   m_Alfa2;
+	float				   m_Alfa3;
+	float				   m_Alfa4;
+	float				   m_Alfa5;
+};
 
 #endif
